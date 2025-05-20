931a5fbf374109ccbb2c80d9afb3bb803b9c3a94
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-20 16:17:58 -0300

    add

--- src/context/CarrinhoContext.js
+++ src/context/CarrinhoContext.js
@@ -13,10 +13,20 @@ export const CarrinhoProvider = ({ children }) => {
         const produtoExistente = carrinho.find(item => item.produtoId === produto._id);
         if(produtoExistente){
             produtoExistente.quantidade += quantidade;
+            setCarrinho([...carrinho])
         } else {
-            
+            setCarrinho([...carrinho, {...produto, quantidade}])
         }
     }
 
+    const removerDoCarrinho = (id) => {
+        setCarrinho(carrinho.filter(item => item.produtoId !== id))
+    }
+
+    return (
+        <CarrinhoContext.Provider>
+            {children}
+        </CarrinhoContext.Provider>
+    )
     
 }
