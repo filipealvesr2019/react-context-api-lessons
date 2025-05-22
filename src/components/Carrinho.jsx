a0faa02504dcd67acda3b22486c1246d1c478725
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-22 15:01:13 -0300

    add

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -7,11 +7,13 @@ export default function Carrinho(){
             <h2>Carrinho</h2>
             <ul>
                 {carrinho.map(item => (
-                    <li>
-                        
+                    <li key={item.produtoId}>
+                       {item.nome} - {item.quantidade} x R${item.preco}
+                       <button onClick={() => removerDoCarrinho(item.produtoId)}>Remover</button>
                     </li>
                 ))}
             </ul>
+            <h3>Total: R${carrinho.reduce((total) => total)}</h3>
         </div>
     )
 }
