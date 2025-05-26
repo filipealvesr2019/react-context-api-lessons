40e30d06c3d6817328618052e0931f907b841255
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-26 15:08:34 -0300

    add

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,7 @@
+export default function ProdutList(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/ProdutoItem..jsx
+++ src/components/ProdutoItem..jsx
@@ -1,6 +1,12 @@
-export default function ProdutoItem(){
+import { useCarrinho } from "@/context/CarrinhoContext"
+
+export default function ProdutoItem({ produto }){
+    const { adicionarAoCarrinho } = useCarrinho();
     return (
         <div>
+            <h3>{produto.nome}</h3>
+            <p>R${produto.preco}</p>
+            <button onClick={() => adicionarAoCarrinho(produto, 1)}>Adicionar Ao Carrinho</button>
             
         </div>
     )
