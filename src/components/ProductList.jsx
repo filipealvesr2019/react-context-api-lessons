995a1d8e5877e75e0e4dfb6c5a7ee17d99f985d6
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-27 15:18:41 -0300

    add

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 export default function ProdutList(){
+    const [produtos, setProdutos] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/produtos`)
+        .then(response => response.json())
+        .then(setProdutos)
+
+    }, [])
     return (
         <div>
-            
+            <h2>Produtos</h2>
+            {produtos.map((produto) => (
+                <ProdutoItem key={produto._id}  produto={produto}/>
+            ))}
         </div>
     )
 }
